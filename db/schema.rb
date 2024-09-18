701eda5273636b0abf84065da4730ed702438d5e
jitera-carrick <hongnhat.nguyen@jitera.com>
2024-09-18 14:11:10 +0700

    main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_17_003634) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_18_041411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,12 +38,32 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_003634) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cases", force: :cascade do |t|
+    t.string "name"
+    t.string "notice"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "column_definitions", force: :cascade do |t|
+    t.bigint "table_definition_id", null: false
+    t.string "name", null: false
+    t.string "name_ja"
+    t.integer "column_type"
+    t.boolean "required"
+    t.boolean "unique"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["table_definition_id"], name: "index_column_definitions_on_table_definition_id"
   end
 
   create_table "creams", force: :cascade do |t|
@@ -62,12 +82,36 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_003634) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "kings", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relations", force: :cascade do |t|
+    t.bigint "table_definition_id", null: false
+    t.integer "relation_type"
+    t.boolean "required"
+    t.integer "related_table_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["table_definition_id"], name: "index_relations_on_table_definition_id"
   end
 
   create_table "simples", force: :cascade do |t|
@@ -82,6 +126,29 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_003634) do
     t.string "name"
     t.string "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "table_definitions", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.string "name", null: false
+    t.string "name_ja"
+    t.string "dashboard"
+    t.string "api"
+    t.string "front"
+    t.boolean "authentication"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_table_definitions_on_project_id"
+  end
+
+  create_table "trucks", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.string "branch"
+    t.string "notice"
+    t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
