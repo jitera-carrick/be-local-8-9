635f592dfb2b51c4062aaa409b87a7045f71ebbd
jitera-carrick <hongnhat.nguyen@jitera.com>
2025-01-14 08:24:38 +0700

    update book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,7 @@ class Book < ApplicationRecord
   validates :uuid, length: { maximum: 255 }
   belongs_to :category
 
+  # 1
+
   # Additional validations for uuid can be added here if needed in the future
 end
